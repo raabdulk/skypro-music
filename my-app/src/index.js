8ab5d509b4e01c9f78f2b856f40301bac21f1a7b
Rashid <raabdulk@gmail.com>
2023-10-01 16:04:36 +0200

    Перенос первого компонента AudioPlayer

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -1,10 +1,11 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import './app.css';
-import Index from './App';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import "./app.css";
+import "./components/AudioPlayer.css";
+import Index from "./App";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <Index />
