35a1ece517b0bc309b4b0be82b99318e26275bb9
Rashid <raabdulk@gmail.com>
2023-10-02 12:25:47 +0200

    Ошибка после установки @babel

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
-import "./app.css";
+import "./App.css";
 import "./components/AudioPlayer.css";
 import "./components/NavMenu.css";
 import "./components/PlayList.css";
